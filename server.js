e23ea2aa68c7c1d47e48483a2c5c22d37fb85d8c
David Castro Castaneda <dcastrocast@gmail.com>
2020-07-16 11:24:01 -0700

    routes for notes and index.html create

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// init app and port defined
 const app = express();
 const PORT = process.env.Port || 3000;
 
+// to start port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
